910e620d1fc488ac6d4b0f7ae7e7301ed85fcd69
Karl Heinz Marbaise <kama@soebes.de>
2024-02-29 18:42:24 +0100

    Added another example.

---
+++ src/test/java/com/soebes/jdk22/gatherer/sealed/KeepOnlyTest.java
@@ -0,0 +1,56 @@
+package com.soebes.jdk22.gatherer.sealed;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Gatherer;
+
+class KeepOnlyTest {
+
+  sealed interface Element permits CustomElement, KnownElement { }
+  sealed interface KnownElement extends Element { }
+  non-sealed interface CustomElement extends Element { }
+  sealed interface HtmlElement extends KnownElement { }
+  sealed interface InternalElement extends KnownElement { }
+
+  record Text(String value) implements InternalElement { }
+  record HtmlLiteral(String value) implements InternalElement { }
+
+  record Div(String value) implements HtmlElement { }
+  record Paragraph(String value) implements HtmlElement { }
+  record Span(String value) implements HtmlElement { }
+  record Image(String value) implements HtmlElement { }
+  record Anchor(String value) implements HtmlElement { }
+
+  record FirstCurstom(String value) implements CustomElement { }
+
+
+  private static final List<Element> ELEMENT_LIST = List.of(
+      new FirstCurstom("FirstCustom"),
+      new Div("Div1"),
+      new Paragraph("P1"),
+      new Span("Span1"),
+      new Image("Image1"),
+      new Anchor("https://google.de"),
+      new Div("Div2"),
+      new HtmlLiteral("Literal1"),
+      new Text("Text1")
+  );
+
+  static <T> Gatherer<T, ?, T> keepOnly(Class<? extends Element> clazz) {
+    Gatherer.Integrator<Void, T, T> integrator =
+        (_, element, downstream) -> {
+          if (clazz.isInstance(element)) {
+            downstream.push(element);
+          }
+          return true;
+        };
+    return Gatherer.ofSequential(integrator);
+  }
+
+  @Test
+  void filterWithGathererInteger() {
+    var result = ELEMENT_LIST.stream().gather(keepOnly(Div.class)).toList();
+    System.out.println("result = " + result);
+  }
+}
