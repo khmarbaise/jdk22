d18dc5f5746d551b2a70563225ed54cbb07e98e9
Karl Heinz Marbaise <kama@soebes.de>
2024-06-12 20:29:57 +0200

    Fixed format.

--- src/test/java/com/soebes/jdk22/collector/TestingCollectorTest.java
+++ src/test/java/com/soebes/jdk22/collector/TestingCollectorTest.java
@@ -13,12 +13,14 @@ class TestingCollectorTest {
   static <T> TestingCollector<T> partitioningBySize(int size) {
     return new TestingCollector<>(size);
   }
+
   @Test
   void usingNonParallel() {
     var result = IntStream.range(0, 10).boxed().collect(partitioningBySize(3));
     assertThat(result).contains(List.of(0, 1, 2), List.of(3, 4, 5), List.of(6, 7, 8), List.of(9));
     System.out.println("result = " + result);
   }
+
   @Test
   @Disabled("Does currently not work!")
   void parallel() {
